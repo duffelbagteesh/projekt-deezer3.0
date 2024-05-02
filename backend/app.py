cecad6cdae9f9b8039398dad081ed97d1fba0b21
leticia <leticia.tishy@gmail.com>
2024-05-01 22:51:02 -0400

    changes to directories

--- backend/app.py
+++ backend/app.py
@@ -46,10 +46,10 @@ def split_audio():
 
     # Return the actual done split audio
     return jsonify({
-        'vocalsPath': '/public/tracks/audio_vocals.wav',
-        'drumsPath': '/public/tracks/audio_drums.wav',
-        'bassPath': '/public/tracks/audio_bass.wav',
-        'instrumentsPath': '/public/tracks/audio_other.wav',
+        'vocalsPath': '/public/tracks/output_vocals.wav',
+        'drumsPath': '/public/tracks/output_drums.wav',
+        'bassPath': '/public/tracks/output_bass.wav',
+        'instrumentsPath': '/public/tracks/output_other.wav',
     })
 
 
